1e77aaafa7f14624dc524ae1132ea25ccab3caf2
Epitacio <epitacio@csktech.coml.br>
2020-02-10 16:47:12 -0300

    feat: Commit inicial dos arquivos do projeto.

---
+++ src/main/java/com/example/pontointeligente/PontoIntelilgenteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIntelilgenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontoIntelilgenteApplication.class, args);
+    }
+
+}
